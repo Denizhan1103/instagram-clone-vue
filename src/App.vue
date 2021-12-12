bd80e0a87619d0eaefbdb7e14e0eb635762f9f68
dnzonr <denizhanonr2000@gmail.com>
2021-12-12 05:52:21 +0300

    Added main layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Flow/>
 </template>
 
 <script lang="ts">
+import Flow from './views/Flow.vue'
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+
 
 @Options({
   components: {
-    HelloWorld,
+    Flow
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "./styles/app.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Flow.vue
@@ -0,0 +1,22 @@
+<template>
+    <Header/>
+</template>
+
+<script lang="ts">
+    import {Vue,Options} from "vue-class-component"
+    import Header from "../components/Header.vue"
+
+    @Options({
+        components: {
+            Header
+        }
+    })
+
+    export default class Flow extends Vue {
+        a:string="Hello"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
